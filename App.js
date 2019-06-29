7a57518717a2ea2309897ab0bd0443033997c8fb
junlee91 <jlee757@uic.edu>
2019-06-29 16:56:38 -0500

    isLoggedIn part One

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { Ionicons } from "@expo/vector-icons";
-import { AppLoading, Font, Asset } from "expo";
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
 import { Text, View, AsyncStorage } from "react-native";
 import { ApolloProvider } from "react-apollo-hooks";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { persistCache } from "apollo-cache-persist";
+import { ThemeProvider } from "styled-components";
 import ApolloClient from "apollo-boost";
 
 import apolloClientOptions from "./apollo";
+import styles from "./styles";
 
 export default function App() {
   const [loaded, setLoaded] = useState(false);
   const [client, setClient] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
 
   const preLoad = async () => {
     try {
@@ -28,6 +33,12 @@ export default function App() {
         cache,
         ...apolloClientOptions
       });
+      const isLoggedIn = await AsyncStorage.getItem("isLoggedIn");
+      if (isLoggedIn === null || isLoggedIn === false) {
+        setIsLoggedIn(false);
+      } else {
+        setIsLoggedIn(true);
+      }
 
       setLoaded(true);
       setClient(client);
@@ -40,11 +51,13 @@ export default function App() {
     preLoad();
   }, []);
 
-  return loaded && client ? (
+  return loaded && client && isLoggedIn !== null ? (
     <ApolloProvider client={client}>
-      <View>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ThemeProvider theme={styles}>
+        <View>
+          {isLoggedIn === true ? <Text>I'm in</Text> : <Text>I'm out</Text>}
+        </View>
+      </ThemeProvider>
     </ApolloProvider>
   ) : (
     <AppLoading />

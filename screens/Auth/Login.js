ee816c3ecd2dfe6576e382c5e42f12cba274ebdd
junlee91 <jlee757@uic.edu>
2019-07-08 21:04:27 -0500

    Auth Components - Login page

--- screens/Auth/Login.js
+++ screens/Auth/Login.js
@@ -1,16 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
- const View = styled.View`
+import AuthButton from "../../components/AuthButton";
+import AuthInput from "../../components/AuthInput";
+
+const View = styled.View`
   justify-content: center;
   align-items: center;
   flex: 1;
 `;
 
- const Text = styled.Text``;
-
- export default () => (
+export default () => (
   <View>
-    <Text>Log In</Text>
+    <AuthInput value="" placeholder="Email" keyboardType="email-address" />
+    <AuthButton onPress={() => null} text="Log In" />
   </View>
 );

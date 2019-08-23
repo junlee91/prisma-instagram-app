28d441a878aff633558bb32f5e9941147e9d6e89
junlee91 <jlee757@uic.edu>
2019-08-22 21:11:00 -0500

    Apollo Context

--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { useQuery } from "react-apollo-hooks";
+import { gql } from "apollo-boost";
 import styled from "styled-components";
 
 import Loader from "../../components/Loader";
+
+const FEED_QUERY = gql`
+  {
+    seeFeed {
+      id
+      location
+      caption
+      user {
+        id
+        avatar
+        username
+      }
+      files {
+        id
+        url
+      }
+      likeCount
+      isLiked
+      comments {
+        id
+        text
+        user {
+          id
+          username
+        }
+      }
+      createdAt
+    }
+  }
+`;
 
 const View = styled.View`
   justify-content: center;
@@ -9,8 +41,8 @@ const View = styled.View`
   flex: 1;
 `;
 
-export default () => (
-  <View>
-    <Loader />
-  </View>
-);
+export default () => {
+  const { loading, data } = useQuery(FEED_QUERY);
+  console.log(loading, data);
+  return <View>{loading ? <Loader /> : null}</View>;
+};

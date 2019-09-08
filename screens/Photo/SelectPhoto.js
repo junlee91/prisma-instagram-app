cec875c91dcc12060b592287ce73ed9f6c785d31
junlee91 <jlee757@uic.edu>
2019-09-07 19:59:56 -0500

    Upload photo base

--- screens/Photo/SelectPhoto.js
+++ screens/Photo/SelectPhoto.js
@@ -6,6 +6,7 @@ import styled from "styled-components";
 
 import Loader from "../../components/Loader";
 import constants from "../../constants";
+import styles from "../../styles";
 
 const View = styled.View`
   justify-content: center;
@@ -13,14 +14,33 @@ const View = styled.View`
   flex: 1;
 `;
 
-export default () => {
+const Button = styled.TouchableOpacity`
+  width: 100px;
+  height: 30px;
+  position: absolute;
+  right: 5px;
+  top: 15px;
+  background-color: ${styles.blueColor};
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+`;
+
+const Text = styled.Text`
+  color: white;
+  font-weight: 600;
+`;
+
+export default ({ navigation }) => {
   const [loading, setLoading] = useState(true);
   const [hasPermission, setHasPermission] = useState(false);
   const [selected, setSelected] = useState();
   const [allPhotos, setAllPhotos] = useState();
+
   const changeSelected = photo => {
     setSelected(photo);
   };
+
   const getPhotos = async () => {
     try {
       const { assets } = await MediaLibrary.getAssetsAsync({
@@ -36,6 +56,7 @@ export default () => {
       setLoading(false);
     }
   };
+
   const askPermission = async () => {
     try {
       const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
@@ -48,6 +69,11 @@ export default () => {
       hasPermission(false);
     }
   };
+
+  const handleSelected = () => {
+    navigation.navigate("Upload", { photo: selected });
+  };
+
   useEffect(() => {
     askPermission();
   }, []);
@@ -63,7 +89,16 @@ export default () => {
                 style={{ width: constants.width, height: constants.height / 2 }}
                 source={{ uri: selected.uri }}
               />
-              <ScrollView contentContainerStyle={{ flexDirection: "row" }}>
+              <Button onPress={handleSelected}>
+                <Text>Select Photo</Text>
+              </Button>
+
+              <ScrollView
+                contentContainerStyle={{
+                  flexDirection: "row",
+                  flexWrap: "wrap"
+                }}
+              >
                 {allPhotos.map(photo => (
                   <TouchableOpacity
                     key={photo.id}
